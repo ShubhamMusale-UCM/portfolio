7ed7ff2e4e1e473dd224d483242fc73db17bf694
ShubhamMusale-UCM <110331846+ShubhamMusale-UCM@users.noreply.github.com>
2025-02-01 00:19:30 +0530

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -525,7 +525,7 @@ const Portfolio = () => {
             </div>
             <div className="relative rounded-full overflow-hidden border-4 border-white shadow-xl aspect-square">
              <img
-  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNNHXhiplS41SM_hx2eXkE1940OtfvXriHtA&s"
+  src="https://github.com/ShubhamMusale-UCM/portfolio/blob/main/src/publicImages/IMG_20240511_172116~2.jpg?raw=true"
   alt="Profile"
   className="w-full h-full object-cover"
   loading="eager"
